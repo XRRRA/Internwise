687905aab8a51bcd7da2c74f96bf35ad4f4965b3
Pavlon28 <114653759+Pavlon28@users.noreply.github.com>
2023-10-25 18:28:57 +0300

    solved login problem by commenting security dependency

--- src/main/java/com/example/security/CustomUserDetails.java
+++ src/main/java/com/example/security/CustomUserDetails.java
@@ -1,102 +1,102 @@
-package com.example.security;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collection;
-
-public class CustomUserDetails implements UserDetails {
-
-    private Long id;
-    private String userEmail;
-    private String password;
-    private String firstName;
-    private String lastName;
-    private Collection<? extends GrantedAuthority> authorities;
-
-    public CustomUserDetails(Long id, String userEmail, String password, String firstName, String lastName, Collection<? extends GrantedAuthority> authorities) {
-        this.id = id;
-        this.userEmail = userEmail;
-        this.password = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.authorities = authorities;
-    }
-
-    public CustomUserDetails(String userEmail, String password, String firstName, String lastName) {
-        this.userEmail = userEmail;
-        this.password = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
-    public Long getId() {
-        return id;
-    }
-/*    private Collection<? extends GrantedAuthority> mapAuthority(List<UserRole> roleNameList) {
-        return roleNameList.stream()
-                .map(role -> new SimpleGrantedAuthority(role.getRoleName().name()))
-                .collect(Collectors.toList());
-    }*/
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    /*public boolean hasPrivilege(RoleName roleName) {
-        for (GrantedAuthority grantedAuth : getAuthorities()) {
-            if (grantedAuth.getAuthority().contains(roleName.name())) {
-                return true;
-            }
-        }
-        return false;
-    }*/
-
-}
+//package com.example.security;
+//
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import java.util.Collection;
+//
+//public class CustomUserDetails implements UserDetails {
+//
+//    private Long id;
+//    private String userEmail;
+//    private String password;
+//    private String firstName;
+//    private String lastName;
+//    private Collection<? extends GrantedAuthority> authorities;
+//
+//    public CustomUserDetails(Long id, String userEmail, String password, String firstName, String lastName, Collection<? extends GrantedAuthority> authorities) {
+//        this.id = id;
+//        this.userEmail = userEmail;
+//        this.password = password;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.authorities = authorities;
+//    }
+//
+//    public CustomUserDetails(String userEmail, String password, String firstName, String lastName) {
+//        this.userEmail = userEmail;
+//        this.password = password;
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//    }
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return null;
+//    }
+//
+//    @Override
+//    public String getPassword() {
+//        return null;
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return null;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
+//    public Long getId() {
+//        return id;
+//    }
+///*    private Collection<? extends GrantedAuthority> mapAuthority(List<UserRole> roleNameList) {
+//        return roleNameList.stream()
+//                .map(role -> new SimpleGrantedAuthority(role.getRoleName().name()))
+//                .collect(Collectors.toList());
+//    }*/
+//
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    /*public boolean hasPrivilege(RoleName roleName) {
+//        for (GrantedAuthority grantedAuth : getAuthorities()) {
+//            if (grantedAuth.getAuthority().contains(roleName.name())) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }*/
+//
+//}
--- src/main/java/com/example/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/example/service/impl/UserDetailsServiceImpl.java
@@ -1,32 +1,32 @@
-package com.example.service.impl;
-
-import com.example.entities.User;
-import com.example.repository.UserRepository;
-import com.example.security.CustomUserDetails;
-import com.example.service.UserDetailsService;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-
-@Service("customUserDetailsService")
-public class UserDetailsServiceImpl implements UserDetailsService {
-    private final UserRepository userRepository;
-
-    public UserDetailsServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @Override
-    public UserDetails loadUserByUserEmail(String userEmail) {
-        Optional<User> user = userRepository.findByEmail(userEmail);
-        if(user.isEmpty()) {
-            throw new UsernameNotFoundException("User not found with such email:" + userEmail);
-        }
-        return null;
-    }
-
-
-
-}
+//package com.example.service.impl;
+//
+//import com.example.entities.User;
+//import com.example.repository.UserRepository;
+//import com.example.security.CustomUserDetails;
+//import com.example.service.UserDetailsService;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Optional;
+//
+//@Service("customUserDetailsService")
+//public class UserDetailsServiceImpl implements UserDetailsService {
+//    private final UserRepository userRepository;
+//
+//    public UserDetailsServiceImpl(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+//
+//    @Override
+//    public UserDetails loadUserByUserEmail(String userEmail) {
+//        Optional<User> user = userRepository.findByEmail(userEmail);
+//        if(user.isEmpty()) {
+//            throw new UsernameNotFoundException("User not found with such email:" + userEmail);
+//        }
+//        return null;
+//    }
+//
+//
+//
+//}
